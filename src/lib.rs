47c0ae4286486371b8a8c997433a7aa475963bea
Rudra Arora <aroraru@itserv.scss.tcd.ie>
2022-10-11 12:10:17 +0100

    snake initialization

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,40 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>, // body of snake
+}
+
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec![SnakeCell(spawn_index)],
+        }
+    }
+}
+
 #[wasm_bindgen]
 pub struct World {
     width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> World {
-        World { width: 8 }
+        World {
+            width: 8,
+            snake: Snake::new(10),
+        }
     }
     pub fn width(&self) -> usize {
         self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
     }
 }
 
--- www/src/index.js
+++ www/src/index.js
@@ -9,8 +9,42 @@ import init, { World } from "snake-game";
 // })();
 
 init().then(() => {
+  const CELL_SIZE = 30;
+
   const world = World.new();
+  const worldWidth = world.width();
+
   const canvas = document.getElementById("snake-canvas");
   const ctx = canvas.getContext("2d");
-  debugger;
+
+  canvas.width = worldWidth * CELL_SIZE;
+  canvas.height = worldWidth * CELL_SIZE;
+
+  function drawWorld() {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
+    }
+
+    // move to the yth row increasing Mat(i,j) where i=y and then move from j=0 to j= worldWidth*CELL_SIZE
+    //The move to and line to takes the (x,y) as arguement so x is denoted here to be worldWidth*CELL_SIZE
+    // The same happends for columns above where x is used to draw the grid's columns and x moves in the x directions and lines to the bottom of the grid
+
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
+    }
+
+    ctx.lineWidth = 2;
+
+    ctx.stroke();
+  }
+
+  function drawSnake() {
+    const snakeIdx = world.snake_head_idx();
+  }
+
+  drawWorld();
 });
